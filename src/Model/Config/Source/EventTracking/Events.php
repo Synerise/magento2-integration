4e6808427f139274a9df431579b9c3fc407f7a2f
Kamil Kanik <kamil.kanik@synerise.com>
2022-05-05 16:00:36 +0200

    Added event "catalog_product_import_bunch_save_after" to the event tracking configuration page.

--- src/Model/Config/Source/EventTracking/Events.php
+++ src/Model/Config/Source/EventTracking/Events.php
@@ -5,18 +5,19 @@ namespace Synerise\Integration\Model\Config\Source\EventTracking;
 class Events implements \Magento\Framework\Option\ArrayInterface
 {
     const OPTIONS = [
-        'customer_register_success'             => 'Customer registration',
-        'customer_login'                        => 'Customer login',
-        'customer_logout'                       => 'Customer logout',
-        'customer_save_after'                   => 'Customer updated',
-        'checkout_cart_add_product_complete'    => 'Customer added product to cart',
-        'sales_quote_remove_item'               => 'Customer removed product from cart',
-        'sales_quote_save_after'                => 'Cart updated',
-        'checkout_cart_update_items_after'      => 'Cart quantities updated',
-        'sales_order_place_after'               => 'Order updated',
-        'catalog_product_save_after'            => 'Product updated',
-        'catalog_product_delete_after'          => 'Product deleted',
-        'newsletter_subscriber_save_after'      => 'Subscription updated'
+        'customer_register_success'                 => 'Customer registration',
+        'customer_login'                            => 'Customer login',
+        'customer_logout'                           => 'Customer logout',
+        'customer_save_after'                       => 'Customer updated',
+        'checkout_cart_add_product_complete'        => 'Customer added product to cart',
+        'sales_quote_remove_item'                   => 'Customer removed product from cart',
+        'sales_quote_save_after'                    => 'Cart updated',
+        'checkout_cart_update_items_after'          => 'Cart quantities updated',
+        'sales_order_place_after'                   => 'Order updated',
+        'catalog_product_save_after'                => 'Product updated',
+        'catalog_product_delete_after'              => 'Product deleted',
+        'catalog_product_import_bunch_save_after'   => 'Product import',
+        'newsletter_subscriber_save_after'          => 'Subscription updated'
     ];
 
     public function toOptionArray()
