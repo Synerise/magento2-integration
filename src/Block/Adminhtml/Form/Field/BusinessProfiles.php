8b24edf7fb04a15550e16fd099c824ab78862546
Bartosz Sieczka <bartosz.sieczka@synerise.com>
2022-06-01 13:12:51 +0200

    Business Profile mapping

---
+++ src/Block/Adminhtml/Form/Field/BusinessProfiles.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Synerise\Integration\Block\Adminhtml\Form\Field;
+
+use Magento\Backend\Block\Template\Context;
+use Magento\Config\Block\System\Config\Form\Field;
+use Magento\Framework\Data\Form\Element\AbstractElement;
+use Magento\Framework\View\Helper\SecureHtmlRenderer;
+use Magento\Store\Model\ResourceModel\Website\CollectionFactory as WebsiteCollectionFactory;
+use Synerise\Integration\ResourceModel\BusinessProfile\CollectionFactory as BusinessProfileCollectionFactory;
+
+class BusinessProfiles extends Field
+{
+    /**
+     * @var WebsiteCollectionFactory 
+     */
+    protected $websiteCollectionFactory;
+
+    /**
+     * @var BusinessProfileCollectionFactory 
+     */
+    protected $businessProfileCollectionFactory;
+
+    public function __construct(
+        Context $context,
+        WebsiteCollectionFactory $websiteCollectionFactory,
+        BusinessProfileCollectionFactory $businessProfileCollectionFactory,
+        array $data = [],
+        ?SecureHtmlRenderer $secureRenderer = null
+    ) {
+        $this->websiteCollectionFactory = $websiteCollectionFactory;
+        $this->businessProfileCollectionFactory = $businessProfileCollectionFactory;
+
+        $this->setTemplate('Synerise_Integration::form/field/business_profiles.phtml');
+
+        parent::__construct($context, $data, $secureRenderer);
+    }
+
+    protected function _getElementHtml(AbstractElement $element)
+    {
+        $this->setElement($element);
+        return $this->_toHtml();
+    }
+
+    public function getRows()
+    {
+        $values = (array) $this->getElement()->getValue();
+
+        $rows = [];
+        $websitesCollection = $this->websiteCollectionFactory->create();
+        foreach($websitesCollection as $website) {
+            $rows[$website->getId()] = [
+                'id' => $website->getId(),
+                'name' => $website->getName(),
+                'value' => !empty($values[$website->getId()]) ? $values[$website->getId()] : ''
+            ];
+        }
+
+        return $rows;
+    }
+
+    public function getBusinessProfiles()
+    {
+        return $this->businessProfileCollectionFactory->create();
+    }
+
+}
