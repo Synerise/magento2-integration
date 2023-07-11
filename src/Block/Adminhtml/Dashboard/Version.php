e481573005f82b21d48fa5284ac18b5d1c07f283
Bartosz Sieczka <bartosz.sieczka@synerise.com>
2023-07-11 15:12:01 +0200

    Add module version info to dashboard

---
+++ src/Block/Adminhtml/Dashboard/Version.php
@@ -0,0 +1,33 @@
+<?php
+namespace Synerise\Integration\Block\Adminhtml\Dashboard;
+
+use Magento\Backend\Block\Template\Context;
+use Magento\Directory\Helper\Data as DirectoryHelper;
+use Magento\Framework\Json\Helper\Data as JsonHelper;
+use Magento\Framework\Module\ResourceInterface;
+
+class Version extends \Magento\Backend\Block\Template
+{
+    /**
+     * @var ResourceInterface
+     */
+    private $moduleResource;
+
+    public function __construct(
+        ResourceInterface $moduleResource,
+        Context $context,
+        array $data = [], ?JsonHelper
+        $jsonHelper = null,
+        ?DirectoryHelper $directoryHelper = null
+    ) {
+        $this->moduleResource = $moduleResource;
+
+        parent::__construct($context, $data, $jsonHelper, $directoryHelper);
+    }
+
+    public function getVersion()
+    {
+        return $this->moduleResource->getDbVersion('Synerise_Integration');
+        return 'x.x.x';
+    }
+}
